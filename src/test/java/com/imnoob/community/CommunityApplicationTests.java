c61c52e08521b00bcf4d591bf43545591bee45f5
13875780121 <1206734994@qq.com>
2021-04-02 20:17:09 +0800

    first commit

---
+++ src/test/java/com/imnoob/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imnoob.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
