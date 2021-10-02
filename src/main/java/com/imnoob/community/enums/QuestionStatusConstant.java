1718b297be35d363af8391a2750f83b1583ffb28
chengkang <1206734994@qq.com>
2021-10-02 15:00:53 +0800

    [新增]文章状态

--- src/main/java/com/imnoob/community/enums/QuestionStatusConstant.java
+++ src/main/java/com/imnoob/community/enums/QuestionStatusConstant.java
@@ -7,5 +7,11 @@ public class QuestionStatusConstant {
     static public Integer AUDIT_PASS = 3;
     static public Integer AUDIT_FAILED = 4;
 
-
+    static String getQuestionInfo(Integer status){
+        if (status == 1) return "已发布";
+        else if (status == 2) return "审核中";
+        else if (status == 3) return "审核通过";
+        else if (status == 4) return "审核失败";
+        else return "";
+    }
 }
